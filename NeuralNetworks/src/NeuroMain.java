cbb5fb6a53016cfee81d87c5873886c1fb6aa79c
Baylrock <skidi@i.ua>
2016-01-15 13:37:26 +0200

    Struct reworking#4

--- NeuralNetworks/src/NeuroMain.java
+++ NeuralNetworks/src/NeuroMain.java
@@ -1,13 +1,14 @@
+import managers.NeuroManager;
+
 import java.io.Serializable;
 
 /**
  * Created by baylrock on 11.01.2016.
  */
 public class NeuroMain implements Serializable {
-
+    public NeuroManager neuroNet;
     public static void main (String[] args) {
-
+        NeuroMain nm = new NeuroMain();
+        nm.neuroNet = NeuroManager.getInstance();
     }
-
-
 }
