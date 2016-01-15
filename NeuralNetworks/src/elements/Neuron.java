a4b70bd861f25ecad51d2c94a690135308bee5a7
Baylrock <skidi@i.ua>
2016-01-15 13:08:56 +0200

    Struct reworking#3

--- NeuralNetworks/src/elements/Neuron.java
+++ NeuralNetworks/src/elements/Neuron.java
@@ -8,8 +8,9 @@ import java.io.Serializable;
  * Created by baylrock on 11.01.2016.
  */
 public class Neuron implements AbstractNeuron, Serializable {
-
-
+    private float WEIGHT;
+    private int index;
+    private float[] set;
     @Override
     public void setValue(float value) {
 
